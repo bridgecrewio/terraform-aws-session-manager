5e88347be8b971d420e8e27c3c572e358ff1bd77
Chris Mazanec <chris@pristin.io>
2021-03-04 16:28:29 +0100

    expanded module functionality - all changes backwards compatible - cleanup (trailing spaces, formatting, removed unused entities) - all resources created dynamically using `count` or `for_each` depending on module configuration - added outputs descriptions - added new variables/module options to better cover SSM preferences : `bucket_key_prefix`, `idle_session_timeout`, `run_as_default_user`, `shell_profile_windows`, `shell_profile_linux`, `enable_run_as` - added new outputs: `iam_policy_arn`, `document_name`, `document_arn` - updated `README.md` accordingly, added `Requirements`, `Providers` and `Notes` sections - addressed https://github.com/bridgecrewio/terraform-aws-session-manager/issues/2 by removing unused code blocks

--- variables.tf
+++ variables.tf
@@ -1,6 +1,13 @@
 variable "bucket_name" {
   description = "Name prefix of S3 bucket to store session logs"
   type        = string
+  default     = ""
+}
+
+variable "bucket_key_prefix" {
+  description = "Name of S3 sub-folder (prefix)"
+  type        = string
+  default     = ""
 }
 
 variable "log_archive_days" {
@@ -18,6 +25,7 @@ variable "log_expire_days" {
 variable "access_log_bucket_name" {
   description = "Name prefix of S3 bucket to store access logs from session logs bucket"
   type        = string
+  default     = ""
 }
 
 variable "access_log_expire_days" {
@@ -27,7 +35,7 @@ variable "access_log_expire_days" {
 }
 
 variable "kms_key_deletion_window" {
-  description = "Waiting period for scheduled KMS Key deletion.  Can be 7-30 days."
+  description = "Number of days to wait for scheduled KMS Key deletion [7-30]"
   type        = number
   default     = 7
 }
@@ -48,6 +56,30 @@ variable "cloudwatch_log_group_name" {
   description = "Name of the CloudWatch Log Group for storing SSM Session Logs"
   type        = string
   default     = "/ssm/session-logs"
+}
+
+variable "idle_session_timeout" {
+  description = "Number of minutes a user can be inactive before a session ends [1-60]"
+  type        = number
+  default     = 20
+}
+
+variable "run_as_default_user" {
+  description = "OS default user name, if IAM user/role 'SSMSessionRunAs' tag is undefined"
+  type        = string
+  default     = "ssm-user"
+}
+
+variable "shell_profile_windows" {
+  description = "Environment variables, shell preferences, or commands to run when session starts"
+  type        = string
+  default     = ""
+}
+
+variable "shell_profile_linux" {
+  description = "Environment variables, shell preferences, or commands to run when session starts"
+  type        = string
+  default     = ""
 }
 
 variable "tags" {
@@ -84,6 +116,12 @@ variable "enable_log_to_cloudwatch" {
   description = "Enable Session Manager to Log to CloudWatch Logs"
   type        = bool
   default     = true
+}
+
+variable "enable_run_as" {
+  description = "Enable Run As support for Linux instances"
+  type        = bool
+  default     = false
 }
 
 variable "vpc_endpoints_enabled" {
