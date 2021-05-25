b928eac66c79f9f356437251d4b5e22df5b88105
Ronak Patel <ronak_patel00@hotmail.com>
2021-05-25 12:18:30 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -138,6 +138,7 @@ resource "aws_kms_alias" "ssmkey" {
 }
 
 resource "aws_cloudwatch_log_group" "session_manager_log_group" {
+  count = var.enable_log_to_cloudwatch ? 1 : 0  
   name              = var.cloudwatch_log_group_name
   retention_in_days = var.cloudwatch_logs_retention
   kms_key_id        = aws_kms_key.ssmkey.arn
